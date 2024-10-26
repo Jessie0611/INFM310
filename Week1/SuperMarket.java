c40342d2fc428915102a65cdc89bcc91bcdc43bc
Jessie0611~ <imjustme0611@hotmail.com>
2024-10-26 16:42:49 -0400

    Working on SuperMarket

--- Week1/SuperMarket.java
+++ Week1/SuperMarket.java
@@ -1,17 +1,12 @@
 // SuperMarket.java - This program creates a report that lists weekly hours worked 
 // by employees of a supermarket. The report lists total hours for 
 // each day of one week. 
-// Input:  Interactive
-// Output: Report. 
-
+// Input:  Interactive / Output: Report. 
 import javax.swing.*;
-
-
 public class SuperMarket
 {
-	public static void main(String args[]) 
+	public static void main(String args[])
 	{
-		// Declare variables.
 		final String HEAD1 = "WEEKLY HOURS WORKED";
 		final String DAY_FOOTER = "          Day Total ";  // Leading spaces are intentional.
 		final String SENTINEL = "done";		// Named constant for sentinel value. 
@@ -22,7 +17,6 @@ public class SuperMarket
 		String prevDay = ""; 		   	// Previous day of week.
 		boolean done = false;				// loop control
 
-		// Print two blank lines.
 		System.out.println(); 
 		System.out.println(); 
 		// Print heading.
@@ -40,20 +34,26 @@ public class SuperMarket
 			hoursWorkedString = JOptionPane.showInputDialog("Enter hours worked: ");
 			hoursWorked = Integer.parseInt(hoursWorkedString); 
 			prevDay = dayOfWeek;
+			hoursTotal = hoursWorked;
 		}
-		
-		   
 		while(done == false)
 		{	
-			// Implement control break logic here
-		        // Include work done in the dayChange() method
+			dayOfWeek = JOptionPane.showInputDialog("Enter day of week or 'done' to quit: ");
+            if (dayOfWeek.compareTo(SENTINEL) == 0) {
+                done = true;
+		} else {
+			hoursWorkedString = JOptionPane.showInputDialog("Enter hours worked: ");
+			hoursWorked = Double.parseDouble(hoursWorkedString); 
+
 		}
-	
-		System.out.println(DAY_FOOTER + hoursTotal);
+		hoursTotal += hoursWorked;
+		if (hoursTotal > 0) {
+            System.out.println(DAY_FOOTER + hoursTotal);
+        }
 					
 		System.exit(0);
 
 	} // End of main() method.
 	
-} // End of SuperMarket class.
-
+}
+}
