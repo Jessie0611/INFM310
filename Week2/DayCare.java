950c9d29e982fbf877a963ecc943325aab7cc064
Jessie0611~ <imjustme0611@hotmail.com>
2024-11-02 17:04:33 -0400

    Start lab 8.2

---
+++ Week2/DayCare.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class DayCare
+ {
+    public static void main(String args[])
+    {
+        // Declare variables
+        double weeklyRate[][] ={{30.00, 60.00, 88.00, 115.00, 140.00}, {26.00, 52.00, 70.00, 96.00, 120.00}, {24.00, 46.00, 67.00, 89.00, 110.00}, {22.00, 40.00, 60.00, 75.00, 88.00}, {20.00, 35.00, 50.00, 66.00, 84.00}};
+        int numDays;
+        int age;
+        String numDaysString;
+        String ageString;
+        int QUIT = 999;
+        Scanner input = new Scanner(System.in);
+        // This is the work done in the getReady() method
+
+        ageString = JOptionPane.showInputDialog("Enter the age of the child or 999 to quit: ");
+        age = Integer.parseInt(ageString);
+
+        
+    }//Exit main
+}//Exit DayCare
--- Week2/HouseholdSize.java
+++ Week2/HouseholdSize.java
@@ -8,12 +8,11 @@ public class HouseholdSize
 	public static void main(String args[]) 
 	{
 		// Declare variables.
-		
-		final int SIZE = 300;			// Maximum number of household sizes.
+		final int SIZE = 300;	// Maximum number of household sizes.
 		int householdSizes[] = new int[SIZE];   // Array used to store up to 300 household sizes.
 		int x;
 		int limit = SIZE;
-		int householdSize = 0; 
+		int householdSize = 0;
 		String householdSizeString;
 		int pairsToCompare;
 		boolean switchOccurred;
@@ -31,26 +30,37 @@ public class HouseholdSize
 		{
 			// Place value in array.
 			householdSizes[x] = householdSize;
-			// Calculate total of household sizes here
-			
+			sum += householdSize;		// Calculate total of household sizes here
 			x++;    // Get ready for next input item.
 			householdSizeString = JOptionPane.showInputDialog("Enter household size or 999 to quit: ");
 			householdSize = Integer.parseInt(householdSizeString); 
 		}  // End of input loop.
-		
-		
 		// Find the mean
-		
-		// This is the work done in the sortArray() method
-		
-		
+		if (x < SIZE)
+		{
+            mean = sum / x;
+        }
+		// This is the work done in the sortArray() method sort sizes in ascending order using a bubble sort
+		x = 0;
+		while(pairsToCompare.)
+		{
+			if () {
+				
+			}
+			x++;
+		}
 		// This is the work done in the displayArray() method
-		// Print the mean 
+		x = 0;
+		while(x < SIZE)
+		{
+			x++;
 
+		}
 		// Calculate the median
 
-                // Print the median
-
+		System.out.println("Sum: " + sum);
+		System.out.println("Mean: " + mean);
+		System.out.println("Median: " + median);
 		System.exit(0);
 	} // End of main() method.
 } // End of HouseholdSize class.
