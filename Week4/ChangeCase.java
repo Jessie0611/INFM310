f5132d5c33df3d42b7979e37089ea07c4cf6b2d9
Jessie0611~ <imjustme0611@hotmail.com>
2024-11-14 20:46:09 -0500

    Add outline to lab 9-7

--- Week4/ChangeCase.java
+++ Week4/ChangeCase.java
@@ -1,9 +1,16 @@
+//lab 9-7
+//
+
 import javax.swing.*;
 
 public class ChangeCase
 {
 	public static void main(String args[]) 
 	{
+		String sample = "This is a String.";
+		String result;
+		result = sample.toLowerCase();
+		result = sample.toUpperCase();
 
     }
 }
