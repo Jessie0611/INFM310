1a610740c6a3a72c84bc1b3b1225fce37b157a4d
Jessie0611~ <imjustme0611@hotmail.com>
2024-11-21 12:30:55 -0500

    Add week 5 class example

---
+++ Week5/Employee.java
@@ -0,0 +1,41 @@
+//Employee Class Example
+
+public class Employee
+{
+    //data attributes
+    private String lastName;
+    private double hourlyWage;
+    private double weeklyPay;
+
+    //set methods
+    public void setLastName(String name)
+    {
+        lastName = name;
+        return;
+    }
+    public void setHourlyWage(double wage)
+    {
+        hourlyWage = wage;
+        calculateWeeklyPay();
+        return;
+    }
+    private void calculateWeeklyPay()
+    {
+        final double WEEKLY_HOURS = 40;
+        weeklyPay = hourlyWage * WEEKLY_HOURS;
+        return;
+    }
+    //get methods have to be public, interface will return values so not void
+    public String getLastName()
+    {
+        return lastName;
+    }
+    public double getHourlyWage()
+    {
+        return hourlyWage;
+    }
+    public double getWeeklyPay()
+    {
+        return weeklyPay;
+    }
+} //end of employee class
