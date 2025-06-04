0c38934a62562887f99a3c7a73cc882ec52ecce7
Jessie0611 <imjustme0611@hotmail.com>
2025-06-04 17:15:38 -0500

    Basic JS function practice

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
@@ -16,3 +16,23 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+function palindrome(str) {
+  str = str.toLowerCase().replace(/\s+/g, '');
+  return str === str.split('').reverse().join('');
+}
+console.log(palindrome("hannah"));
+console.log(palindrome("racecar"));
+console.log(palindrome("never odd or even"))
+
+
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n)); {
+    if (n % i === 0) {
+      return false;
+    }
+    return true;
+  }
+}
+console.log(isPrime(7));
