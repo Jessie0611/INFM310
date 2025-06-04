2f657b1ff6a17e4fa1c2bbbffd907f33ce5a418e
Jessie0611 <imjustme0611@hotmail.com>
2025-06-04 17:16:36 -0500

    Basic JS function practice

--- script.js
+++ script.js
@@ -28,11 +28,13 @@ console.log(palindrome("never odd or even"))
 
 function isPrime(n) {
   if (n <= 1) return false;
-  for (let i = 2; i <= Math.sqrt(n)); {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
     if (n % i === 0) {
       return false;
     }
-    return true;
   }
+  return true;
 }
-console.log(isPrime(7));
+console.log(isPrime(7));  // true
+console.log(isPrime(4));  // false
+console.log(isPrime(13)); // true
